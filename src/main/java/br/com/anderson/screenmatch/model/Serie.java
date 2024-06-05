f75051eeacce66134f2fc5f4dc3dfdf26535ee47
Anderson-Batista <andersonbfs15@gmail.com>
2024-06-05 17:40:05 -0300

    conexao com banco de dados

--- src/main/java/br/com/anderson/screenmatch/model/Serie.java
+++ src/main/java/br/com/anderson/screenmatch/model/Serie.java
@@ -1,17 +1,28 @@
 package br.com.anderson.screenmatch.model;
 
 import br.com.anderson.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String titulo;
     private  Integer totalTemporadas;
     private  Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private  Categoria genero;
     private  String atores;
     private  String poster;
     private  String sinopse;
+
+    public Serie() {
+    }
 
     public Serie(DadosSerie dadosSerie) {
         this.titulo = dadosSerie.titulo();
@@ -24,6 +35,12 @@ public class Serie {
         this.sinopse = dadosSerie.sinopse();
     }
 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getTitulo() {
         return titulo;
     }
