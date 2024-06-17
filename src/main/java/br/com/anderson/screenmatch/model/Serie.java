acf96a6fd624926f5219f704b8a59b7970ca79e0
Anderson-Batista <andersonbfs15@gmail.com>
2024-06-17 15:50:26 -0300

    Ajuste chave estrangeira

--- src/main/java/br/com/anderson/screenmatch/model/Serie.java
+++ src/main/java/br/com/anderson/screenmatch/model/Serie.java
@@ -23,7 +23,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios;
 
     public Serie() {
@@ -104,11 +104,12 @@ public class Serie {
         this.sinopse = sinopse;
     }
 
-    public List getEpisodios() {
+    public List<Episodio> getEpisodios() {
         return episodios;
     }
 
-    public void setEpisodios(List episodios) {
+    public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -121,6 +122,7 @@ public class Serie {
                         ", avaliacao=" + avaliacao +
                         ", atores='" + atores + '\'' +
                         ", poster='" + poster + '\'' +
-                        ", sinopse='" + sinopse + '\'';
+                        ", sinopse='" + sinopse + '\'' +
+                        ", episodios='" + episodios + '\'';
     }
 }
